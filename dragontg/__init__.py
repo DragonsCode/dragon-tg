d4967e2521eeaf58849be7de213e4fe90e92aebc
DragonsCode <dilimmurod@gmail.com>
2025-03-04 11:09:56 +0500

    Added setup and gitignore. Updated init file

--- dragontg/__init__.py
+++ dragontg/__init__.py
@@ -1,1 +1,13 @@
 from . import models, handlers, methods
+
+"""
+dragontg
+
+My telegram bot library from 0.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Murodov Dilmurod'
+__credits__ = 'dragonscode.uz'
+
+__all__ = ['models', 'handlers', 'methods']
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dragontg',
+    version='0.0.1',
+    description='My telegram bot library from 0.',
+    url='git@github.com:DragonsCode/dragon-tg.git',
+    author='Murodov Dilmurod',
+    author_email='dilmurodmurodov978@gmail.com',
+    license='MIT',
+    packages=['dragontg'],
+    zip_safe=False
+)
